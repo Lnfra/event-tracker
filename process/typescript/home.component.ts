36bbac6f57e6514a47f2897bf24bb4d2a51412ab
Angeline Tan <atan009@gmail.com>
2016-05-05 23:04:53 +0800

    Added component for home page

--- process/typescript/home.component.ts
+++ process/typescript/home.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from 'angular2/core';
 
 @Component({
   selector: 'home-page',
-  template: `<h1>Home</h1>`
+  templateUrl: 'templates/home.component.html'
 })
 
 export class HomeComponent {
